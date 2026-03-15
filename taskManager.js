112ff3ae3c6fa1ceaa0da46549736b58d37ba1c9
Ihalake <halakeissene@gmail.com>
2026-03-15 23:23:14 +0300

    Add core task manager logic

---
+++ taskManager.js
@@ -0,0 +1,30 @@
+// create a function to handle the tasks - add ,edit, delete,list,status
+function createTaskManager(){
+    let tasks =[];
+    return{
+        addTask(name,status="ToDo",deadline){
+            const newTask = {
+                id: Date.now(),
+                name: name,
+                status: status,
+                deadline: deadline
+
+
+            };
+            tasks.push(newTask); // added new task to my private array tasks
+            return newTask;
+        },
+       
+                    
+        listTasks() {
+            return [...tasks];
+        },
+        removeTask(id){// create a new array without the removed task
+            tasks = tasks.filter(task => task.id !== id);
+         
+        },              
+
+    }
+
+
+};
